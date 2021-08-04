f0c684a1f2662a504ddabab40b7af38639048d32
Paul Khlopkov <mainguy@mail.ru>
2021-08-04 23:51:04 +0300

    Added sidebar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,24 @@
+// System
+// import './toggler'
 
 // UIKit
 import './uikit'
 import './uikit.icons'
 
-// System
+
+// () => {
+//     const menuToggler = document.getElementById('menu-toggler')
+//     const menuWrapper = document.getElementById('mobile-menu')
+//     if (menuToggler.click()) {
+//         if (menuWrapper.classList.contains('uk-open')) {
+//             menuToggler.classList.add('show')
+//         } else {
+//             menuToggler.classList.remove('show')
+//         }
+//     }
+// }
+
+
+
+
+
---
+++ assets/js/toggler.js
@@ -0,0 +1,5 @@
+showMenu = () => {
+    document.getElementById('mobile-menu').classList.toggle("show")
+    document.querySelector('.is-navbar__wrapper-left__toggler').classList.toggle('show')
+    console.log('Yeah')
+}
