285e077ca058cf848da3a808d3ed4bbb39f47b46
Paul Khlopkov <mainguy@mail.ru>
2021-08-29 00:32:44 +0300

    Fixed mobile menu sidebar script

--- src/js/MobileMenuToggler.js
+++ src/js/MobileMenuToggler.js
@@ -1,4 +1,6 @@
 showMenu = () => {
-    document.getElementById('mobile-menu').classList.toggle("show")
-    document.querySelector('.is-navbar__wrapper-left__toggler').classList.toggle('show')
+    const MobileMenuWrapper = document.getElementById('mobile-menu')
+    const MobileMenuToggler = document.querySelector('.is-navbar__wrapper-left__toggler')
+    MobileMenuWrapper.classList.toggle("show")
+    MobileMenuToggler.classList.toggle('show')
 }
