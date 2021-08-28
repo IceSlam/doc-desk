36bd82442874abe4e5a4ba022eb3ddbd83756b8b
Paul Khlopkov <mainguy@mail.ru>
2021-08-29 00:08:29 +0300

    Replaced Scroll to top script to single file

---
+++ src/js/scrolltop.js
@@ -0,0 +1,24 @@
+(function() {
+    'use strict';
+
+    var scrollTopBtn = document.querySelector('.scroll-to-top');
+    function topScroll() {
+        let scrolled = window.pageYOffset;
+        let coords = document.documentElement.clientHeight;
+
+        if (scrolled > coords) {
+            scrollTopBtn.classList.add('show');
+        }
+        if (scrolled < coords) {
+            scrollTopBtn.classList.remove('show');
+        }
+    }
+
+    function scrollToTop() {
+        if (window.pageYOffset > 30) {
+            window.scrollBy(0, -40);
+            setTimeout(scrollToTop, 0);
+        }
+    }
+    window.addEventListener('scroll', topScroll);
+})();
